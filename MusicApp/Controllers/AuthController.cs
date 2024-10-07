6b1bbe180e9b2d5bae37c5655128d4ba5353ef23
thotocdonm <minhson411@gmail.com>
2024-10-07 16:42:02 +0700

    init project

---
+++ MusicApp/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
