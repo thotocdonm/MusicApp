dd632dfbdeb20180ff55a8f54a6946bf5754b082
thotocdonm <minhson411@gmail.com>
2024-10-12 14:09:02 +0700

    init project structure

---
+++ MusicApp/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult User()
+        {
+            return View();
+        }
+
+        public ActionResult Music()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
